8bf94d2e353d82fa608ac50647fe3581af759e1e
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-06-10 00:06:40 +0530

    fix: removed shell script

--- shell.js
+++
@@ -1,8 +0,0 @@
-var shell = require("shelljs");
-
-shell.echo("hello world");
-shell.exec("commitlint -E GIT_PARAMS", function(code, stdout, stderr) {
-  console.log("Exit code:", code);
-  console.log("Program output:", stdout);
-  console.log("Program stderr:", stderr);
-});
