5f5b36cde4bec4028475bddd8621527ce437f6a0
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-06-09 23:48:10 +0530

    testing shell

--- shell.js
+++ shell.js
@@ -1,4 +1,8 @@
 var shell = require("shelljs");
 
 shell.echo("hello world");
-shell.exec("commitlint -E GIT_PARAMS");
+shell.exec("commitlint -E GIT_PARAMS", function(code, stdout, stderr) {
+  console.log("Exit code:", code);
+  console.log("Program output:", stdout);
+  console.log("Program stderr:", stderr);
+});
